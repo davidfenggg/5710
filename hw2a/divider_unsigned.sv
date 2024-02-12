9af68ae034bf7343a9fc40d9052c811a5d9c17d5
Victoria Zammit <vszammit@gmail.com>
2024-02-12 05:25:09 +0000

    fixing hw 2a

--- hw2a/divider_unsigned.sv
+++ hw2a/divider_unsigned.sv
@@ -1,4 +1,4 @@
-/* INSERT NAME AND PENNKEY HERE */
+/* Victoria Zammit - vzammit; David Feng - dfenggg */
 
 `timescale 1ns / 1ns
 
@@ -10,9 +10,31 @@ module divider_unsigned (
     output wire [31:0] o_remainder,
     output wire [31:0] o_quotient
 );
-    // for (0 to 31)
-    // TODO: your code here
+    wire [31:0] remainder_wires[0:32]; 
+    wire [31:0] quotient_wires[0:32];  
+    wire [31:0] dividend_wires[0:32];
 
+    assign remainder_wires[0] = 0;
+    assign quotient_wires[0] = 0;
+    assign dividend_wires[0] = i_dividend;
+
+    genvar i;
+    generate
+        for (i = 0; i < 32; i++) begin : divu_iter
+            divu_1iter div_step(
+                .i_dividend(dividend_wires[i]), 
+                .i_divisor(i_divisor),
+                .i_remainder(remainder_wires[i]),
+                .i_quotient(quotient_wires[i]),
+                .o_dividend(dividend_wires[i+1]),
+                .o_remainder(remainder_wires[i + 1]),
+                .o_quotient(quotient_wires[i + 1])
+            );
+        end
+    endgenerate
+
+    assign o_remainder = remainder_wires[32];
+    assign o_quotient = quotient_wires[32];
 endmodule
 
 
@@ -25,31 +47,16 @@ module divu_1iter (
     output wire [31:0] o_remainder,
     output wire [31:0] o_quotient
 );
-    /*
-    for (int i = 0; i < 32; i++) {
-        remainder = (remainder << 1) | ((dividend >> 31) & 0x1);
-        if (remainder < divisor) {
-            quotient = (quotient << 1);
-        } else {
-            quotient = (quotient << 1) | 0x1;
-            remainder = remainder - divisor;
-        }
-        dividend = dividend << 1;
-    }
-    */
-    wire [31:0] shifted_remainder;
+    wire[31:0] shifted_remainder = (i_remainder << 1) | ((i_dividend >> 31) & 32'b1);
 
-    always_comb begin
-        shifted_remainder = {i_remainder << 1} | (i_dividend >> 31);
-        if(shifted_remainder < i_divisor) begin
-            o_quotient = (i_quotient << 1);
-            o_remainder = shifted_remainder;
-        end
-        else begin
-            o_quotient = (i_quotient << 1) | 4'b1;
-            o_remainder = shifted_remainder - i_divisor;
-        end
-        assign o_dividend = i_dividend << 1;
-    end
-
+    //if remainder < divisor, then quotient = (quotient << 1)
+    //else remainder = remainder - divisor
+    assign o_remainder = (shifted_remainder < i_divisor) ? shifted_remainder : shifted_remainder - i_divisor;
+    
+    //if remainder < divisor, then quotient = (quotient << 1)
+    //else quotient = (quotient << 1) | 0x1
+    assign o_quotient = (shifted_remainder < i_divisor) ? (i_quotient << 1) : ((i_quotient << 1) | 32'b1);
+    
+    //update dividend 
+    assign o_dividend = i_dividend << 1;
 endmodule
